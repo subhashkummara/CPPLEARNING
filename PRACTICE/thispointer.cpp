5306b6a36043eb61b1f9634e254e194d22840e6e
Kummara Subhash (MS/EDB3-XC) <Subhash.Kummara@in.bosch.com>
2023-09-23 15:55:20 +0530

    used this pointer here for the inmplimentation of thisponter

--- PRACTICE/thispointer.cpp
+++ PRACTICE/thispointer.cpp
@@ -3,23 +3,28 @@ using namespace std;
 
 class myclass
 {
-public:
+private:
     int subhash;  // Declare subhash without initialization
-
+public:
     myclass() {
         subhash = 0;  // Initialize subhash in the constructor
     }
 
-    int returnsubhash(int x){
-        subhash  = x;
-        return subhash;
+    void setsubhash(int x){
+        this->subhash = x++;  // Removed the post-increment (x++), as it's not needed
+    }
+
+    int getsubhash(){
+        return this->subhash;  // Return subhash directly, not *this
     }
 };
 
 int main(){
     myclass oneclass;
     int x;
-    x = oneclass.returnsubhash(4);
+    oneclass.setsubhash(4);
+    x = oneclass.getsubhash();  // Added a semicolon here
     cout << x << endl;
+
     return 0;
 }
